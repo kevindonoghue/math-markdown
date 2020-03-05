75566384927d75e647cfba1a8ce2bcb1e7444d87
Kevin Donoghue <kjdmath@gmail.com>
2020-03-05 15:06:05 -0800

    rewrite Plot to increase load times

--- src/components/Plot.js
+++ src/components/Plot.js
@@ -1,23 +1,64 @@
 import React, { useEffect, useRef, useState } from "react";
 import PlotlyPlot from "react-plotly.js";
+import { InView } from "react-intersection-observer";
+
 
 // wraps the Plot component from react-plotly.js with an observer that triggers a rerender if the plot is in view but its WebGL context has been lost
 function Plot(props) {
-  const { data, layout, config, three, style } = props;
+  const { three } = props;
+  if (!three) {
+    return <Plot2d {...props} />;
+  }
+
+  // need to go back and change this to match the correct height and width passed down from props
+  const placeholderStyle = { height: "400px", width: "400px" };
+
+  // using inView instead of an intersection observer since I had some trouble getting the intersection observer to work
+  const [comp, setComp] = useState(
+    <InView as="div" onChange={handleInitialView}>
+      <div style={placeholderStyle}></div>
+    </InView>
+  );
+
+  function handleInitialView(inView) {
+    if (inView) {
+      setComp(<Plot3d {...props} />);
+    }
+  }
+
+  return comp;
+}
+
+Plot.defaultProps = {
+  data: [],
+  layout: {},
+  config: {},
+  three: false
+};
+
+function Plot2d(props) {
+  const { data, layout, config, style } = props;
+  return (
+    <PlotlyPlot data={data} layout={layout} config={config} style={style} />
+  );
+}
+
+function Plot3d(props) {
+  const { data, layout, config, style } = props;
   const ref = useRef();
   const [key, setKey] = useState(0);
 
+  // I don't use inView here because this is, to my eye, a little faster
   useEffect(() => {
-    if (!three) {
-      return;
-    }
-
     function handleIntersection([entry]) {
       if (entry.isIntersecting) {
         // the react-plotly.js component Plot creates a particular set of nested dom elements; here we find the canvas in this set of dom elements
-        const canvas = ref.current
-          .querySelector(".gl-container")
-          .querySelector("canvas");
+        const glContainer = ref.current
+        .querySelector(".gl-container")
+        if (!glContainer) {
+          return
+        }
+        const canvas = glContainer.querySelector("canvas");
         if (canvas.getContext("webgl").isContextLost()) {
           setKey(key => key + 1); // passing a function to a state setter allows you to change the state based on its previous value
         }
@@ -37,15 +78,7 @@ function Plot(props) {
         config={config}
         style={style}
       />
-    </div>
-  );
+    </div>);
 }
-
-Plot.defaultProps = {
-  data: [],
-  layout: {},
-  config: {},
-  three: false
-};
 
 export default Plot;
